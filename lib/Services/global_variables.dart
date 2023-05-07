3f3720ff68b4ae881b9f604ace9dcfd013b07f82
ritikdeveloper <ritikdeveloper@gm ail.com>
2023-05-07 21:49:27 +0530

    first commit

---
+++ lib/Services/global_variables.dart
@@ -0,0 +1,9 @@
+String loginUrlImage =
+    'https://images.pexels.com/photos/269077/pexels-photo-269077.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1.jpg';
+String signUpUrlImage =
+    'https://images.pexels.com/photos/443383/pexels-photo-443383.jpeg';
+String forgetUrlImage =
+    'https://images.pexels.com/photos/9578527/pexels-photo-9578527.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1';
+String? name = '';
+String? userImage = '';
+String? location = '';
